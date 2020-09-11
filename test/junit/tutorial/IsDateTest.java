5f9e766b1c59658b97ef92cf2c52211c03ea5ca7
KenjiOyamada <oyaken0717@yahoo.co.jp>
2020-09-11 10:59:19 +0900

    【first commit】developブランチに作成予定

---
+++ test/junit/tutorial/IsDateTest.java
@@ -0,0 +1,17 @@
+package junit.tutorial;
+
+import static org.junit.Assert.assertThat;
+
+import java.util.Date;
+
+import org.junit.Test;
+
+public class IsDateTest {
+
+	@Test
+	public void 日付の比較() throws Exception{
+		Date date = new Date();
+		assertThat(date, is(dateOf(2011,2,10)));
+	}
+
+}
